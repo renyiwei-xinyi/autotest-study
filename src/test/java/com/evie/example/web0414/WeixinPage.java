be8b9aef3edfc2b03aca430bd2b2996da8621f81
Evie <renyiwei1129@163.com>
2021-04-16 16:58:59 +0800

    完成实战练习

--- src/test/java/com/evie/example/web0414/WeixinPage.java
+++ src/test/java/com/evie/example/web0414/WeixinPage.java
@@ -3,19 +3,42 @@ package com.evie.example.web0414;
 import com.evie.autotest.atom.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+
+import java.util.List;
 
 public class WeixinPage extends BasePage {
     public WeixinPage(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy
-    public WebElement search_button;
+    //================================ 可被点击按钮元素 ==================
+    @FindBy(id = "menu_contacts") public WebElement menu_contacts;
 
-    @FindBy
-    public WebElement search_term;
+    @FindBy(linkText = "添加成员") public WebElement add_member;
 
-    @FindBy
-    public WebElement search_text;
+    @FindBy(id = "logout") public WebElement logout;
+
+    @FindBy(name = "sendInvite") public WebElement sendInvite;
+
+    @FindBy(linkText = "保存") public WebElement save;
+
+    @FindBy(name = "extern_position") public WebElement extern_position;
+
+    @FindBy(className = "ww_radio")
+    public List<WebElement> button;
+
+
+    //================================ 输入框元素 ==================
+    @FindBy(id = "username") public WebElement username;
+    @FindBy(id = "memberAdd_english_name") public WebElement memberAdd_english_name;
+    @FindBy(id = "memberAdd_acctid") public WebElement memberAdd_acctid;
+    @FindBy(id = "memberAdd_phone") public WebElement memberAdd_phone;
+    @FindBy(id = "memberAdd_telephone") public WebElement memberAdd_telephone;
+    @FindBy(id = "memberAdd_mail") public WebElement memberAdd_mail;
+    @FindBy(id = "memberEdit_address") public WebElement memberEdit_address;
+    @FindBy(id = "memberAdd_title") public WebElement memberAdd_title;
+
 }
--- src/test/java/com/evie/example/web0414/WeixinloginTest.java
+++ src/test/java/com/evie/example/web0414/WeixinloginTest.java
@@ -1,23 +1,29 @@
 package com.evie.example.web0414;
 
 import com.evie.autotest.annotation.JsonFileSource;
+import com.evie.autotest.annotation.YamlFileSource;
 import com.evie.autotest.interfaces.DriverStart;
 import com.evie.autotest.interfaces.TimeExecutionLogger;
 import com.evie.autotest.util.JsonLogUtils;
-import org.apache.commons.io.IOUtils;
+import lombok.Data;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WeixinloginTest implements DriverStart, TimeExecutionLogger  {
 
@@ -29,13 +35,18 @@ public class WeixinloginTest implements DriverStart, TimeExecutionLogger  {
 
     private static ChromeDriver driver;
 
+    private static WeixinPage page;
+
     @BeforeAll
     static void before_all(){
         // 谷歌浏览器实例
         driver = new ChromeDriver(new ChromeOptions().setHeadless(false));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        // 初始化页面元素
+        page = PageFactory.initElements(driver, WeixinPage.class);
 
     }
-    @AfterAll
+    //@AfterAll
     static void after_all(){
         // 测试结束 关闭浏览器
         driver.close();
@@ -55,9 +66,13 @@ public class WeixinloginTest implements DriverStart, TimeExecutionLogger  {
     }
 
 
+    /**
+     * 课后作业1：自动登录企业微信
+     * @param cookies 已经保存的，未过期的cookie
+     */
     @JsonFileSource(files = COOKIE)
     @ParameterizedTest
-    void test_2_login(List<HashMap<String, String>> cookies){
+    void test_2_login(List<Map<String, String>> cookies){
         // 打开网页
         driver.get(URL);
 
@@ -69,8 +84,52 @@ public class WeixinloginTest implements DriverStart, TimeExecutionLogger  {
         });
         //导航栏 刷新操作
         driver.navigate().refresh();
-        // 打印 页面title
-        LOGGER.info(driver.getTitle());
+        // 登录成功 有退出 按钮 校验是否可见
+        assertTrue(page.logout.isDisplayed(),"登录失败！请重新手动获取存储cookie");
+    }
+
+    @Data
+    static class memberInfo{
+
+        private String name;
+        private String alias;
+        private String id;
+        private String phone;
+        private String landline;
+        private String email;
+        private String address;
+        private String jobTitle;
+    }
+
+    /**
+     * 课后作业2：自动添加公司成员
+     * @param date
+     */
+    @YamlFileSource(files = "/example/web0414/memberInfo.yaml")
+    @ParameterizedTest
+    void test_3_add_member(Object date){
+        memberInfo memberInfo = JsonLogUtils.jsonTo(date, memberInfo.class);
+        assert memberInfo != null;
+        // 点击通讯录
+        page.menu_contacts.click();
+        // 点击添加成员
+        page.add_member.click();
+        // 输入成员信息
+        page.username.sendKeys(memberInfo.getName());
+        page.memberAdd_english_name.sendKeys(memberInfo.getAlias());
+        page.memberAdd_acctid.sendKeys(memberInfo.getId());
+        page.memberAdd_phone.sendKeys(memberInfo.getPhone());
+        page.memberAdd_telephone.sendKeys(memberInfo.getLandline());
+        page.memberAdd_mail.sendKeys(memberInfo.getEmail());
+        page.memberEdit_address.sendKeys(memberInfo.getAddress());
+        page.memberAdd_title.sendKeys(memberInfo.getJobTitle());
+
+        page.button.forEach(WebElement::click);
+        page.extern_position.sendKeys("自定义职务");
+
+        page.sendInvite.click();
+
+        page.save.click();
     }
 
 
