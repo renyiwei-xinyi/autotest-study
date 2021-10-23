599923318c4cd5d8dc97d34de3bea127777bf237
Evie <renyiwei1129@163.com>
2021-10-23 15:27:36 +0800

    up

--- src/test/java/com/evie/autotest/TestOkhttp3.java
+++ src/test/java/com/evie/autotest/TestOkhttp3.java
@@ -3,20 +3,79 @@ package com.evie.autotest;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.util.ReUtil;
 import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.evie.autotest.provider.JsonFileSource;
 import com.evie.autotest.atom.api.WorkWeiXin;
 import com.evie.autotest.util.HttpUtils;
+import com.evie.autotest.util.JsonUtils;
 import okhttp3.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class TestOkhttp3 {
 
     final static String url = "http://www.baidu.com";
 
+    final static String url_test = "https://miniapps.ryxsg.qq.com:8080/xsg_prod/minigame/wxUpdateItemData";
+    final static String url_test_app_1 = "https://miniapps.ryxsg.qq.com:8080/xsg_prod/minigame/wxUpdatePointData";
+    final static String url_test_app_2 = "https://miniapps.ryxsg.qq.com:8080/xsg_prod/minigame/wxPointsToItems";
+
+
+    // 0 一天可以刷一次
+    // 1 一天可以刷五十多个 add 2 / 5
+    @JsonFileSource(files = "/test/addt0.json")
+    void test_12123(Map data) throws InterruptedException {
+
+        HttpUrl url = HttpUtils
+                .getBaseUrl(url_test)
+                .build();
+        HttpUrl url_1 = HttpUtils
+                .getBaseUrl(url_test_app_1)
+                .build();
+        HttpUrl url_2 = HttpUtils
+                .getBaseUrl(url_test_app_2)
+                .build();
+
+        Headers headers = new Headers
+                .Builder()
+                .add("User-Agent", "Mozilla/5.0 (Linux; Android 5.1.1; vivo X7 Build/LMY47V; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3135 MMWEBSDK/20210601 Mobile Safari/537.36 MMWEBID/2438 MicroMessenger/8.0.11.1980(0x28000B59) Process/appbrand1 WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android")
+                .add("Referer", "https://servicewechat.com/wx3c8c1309511bca2f/14/page-frame.html")
+                .add("Accept-Encoding", "gzip,compress,br,deflate")
+                .add("content-type", "application/json")
+                .add("charset", "utf-8")
+                .add("Connection", "keep-alive")
+                .add("Host", "miniapps.ryxsg.qq.com:8080")
+                .build();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.set("token", data.get("token"));
+        jsonObject.set("add", 10);
+        jsonObject.set("way", -100);
+        int i = 1989;
+        int item = 0;
+        while (item < i){
+            String post = HttpUtils.post(url, headers, jsonObject);
+//            HttpUtils.post(url_1, headers, jsonObject);
+//            String post = HttpUtils.post(url_2, headers, data);
+            JSON parse = JSONUtil.parse(post);
+            if (parse.getByPath("data") == null){
+                jsonObject.set("way", Integer.valueOf(jsonObject.get("way").toString()) + 1);
+                continue;
+            }
+            String s = parse.getByPath("data.item").toString();
+            System.out.println("令牌数为：" + s);
+            item = item + Integer.parseInt(s);
+            Thread.sleep(500);
+        }
+
+
+
+    }
     /**
      * 同步get请求
      */
@@ -91,10 +150,8 @@ public class TestOkhttp3 {
         }
     }
 
-    @Test
-    void test_12123(){
 
-    }
+
 
 
     @JsonFileSource(files = "/test/demo.json")
